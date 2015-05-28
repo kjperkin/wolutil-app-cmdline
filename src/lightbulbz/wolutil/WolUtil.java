453390343fd920710e7386adfb85055575c142bf
Kevin Perkins <kevin_perkins2@yahoo.com>
2015-05-28 13:35:12 -0700

    Working utility to send WOL packets

--- src/lightbulbz/wolutil/WolUtil.java
+++ src/lightbulbz/wolutil/WolUtil.java
@@ -1,15 +1,58 @@
 package lightbulbz.wolutil;
 
 import lightbulbz.net.MacAddress;
+import lightbulbz.net.MacAddressFormatException;
 
+import java.io.IOException;
 import java.net.*;
+import java.util.Collection;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
 
 public class WolUtil
 {
+    private static final byte FF = (byte)0xff;
+    private static final byte[] PAYLOAD_START = {FF, FF, FF, FF, FF, FF};
+    private static final int WOL_PACKET_SIZE = 6 + 16*6;
+
     public static void main(String[] argv) {
+        if (argv.length != 1) {
+            usage();
+            System.exit(1);
+        }
+
+        MacAddress targetMac = null;
+        try {
+            targetMac = MacAddress.parseMacAddress(argv[0]);
+        } catch (MacAddressFormatException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+
+        if (targetMac != null) {
+            System.out.println("Target MAC address is " + targetMac.toString());
+            for (InetAddress addr : getBroadcastAddresses()) {
+                System.out.println("Sending WOL packet to broadcast address " + addr.toString().replaceFirst("^[^/]*/", ""));
+                for (int j = 0; j < 10; j++) {
+                    try {
+                        sendWolPacket(addr, targetMac);
+                        System.out.print('.');
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            // ignore
+                        }
+                    } catch (IOException e) {
+                        System.out.print('\n');
+                        System.out.print("Warning: IOException when sending WOL packet:  " + e.getMessage());
+                    }
+                }
+                System.out.print('\n');
+            }
+        }
+    }
+
+    private static Collection<InetAddress> getBroadcastAddresses() {
         Set<InetAddress> broadcastAddresses = new HashSet<>();
         try {
             Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
@@ -31,12 +74,24 @@ public class WolUtil
         for (InetAddress addr : broadcastAddresses) {
             System.out.println(addr);
         }
-        MacAddress amac = new MacAddress(new byte[]{(byte)0xDE, (byte)0xAD, (byte)0xBE, (byte)0xEF, (byte)0x00, (byte)0xFF});
-        System.out.println(amac);
+        return broadcastAddresses;
     }
 
-    public void SendWolPacket(MacAddress mac) throws SocketException {
+    public static void usage() {
+        System.out.println("Usage: wolutil <mac_address>");
+    }
+
+
+    public static void sendWolPacket(InetAddress targetAddr, MacAddress targetMac) throws IOException {
         DatagramSocket mySock = new DatagramSocket();
+        byte[] packetBytes = new byte[WOL_PACKET_SIZE];
+        byte[] macBytes = targetMac.getAddressBytes();
+        System.arraycopy(PAYLOAD_START, 0, packetBytes, 0, PAYLOAD_START.length);
+        for (int i = PAYLOAD_START.length; i < WOL_PACKET_SIZE; i+= macBytes.length) {
+            System.arraycopy(macBytes, 0, packetBytes, i, macBytes.length);
+        }
+        SocketAddress addr = new InetSocketAddress(targetAddr, 9);
+        mySock.send(new DatagramPacket(packetBytes, packetBytes.length, addr));
     }
 }
 
